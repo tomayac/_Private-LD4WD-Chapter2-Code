f744e1cca8242c2114e2a03bc3d7e9a246cd3ea5
Arnaud Brousseau <arnaudb@google.com>
2011-03-09 18:03:43 +0100

    (Almost) complete rewrite of the app with modular approach.

---
+++ js/app.js
@@ -0,0 +1,139 @@
+/***********************/
+/* URL Encode / Decode */
+/***********************/
+//Source : http://plugins.jquery.com/project/URLEncode
+$.extend({URLEncode:function(c){var o='';var x=0;c=c.toString();var r=/(^[a-zA-Z0-9_.]*)/;
+  while(x<c.length){var m=r.exec(c.substr(x));
+    if(m!=null && m.length>1 && m[1]!=''){o+=m[1];x+=m[1].length;
+    }else{if(c[x]==' ')o+='+';else{var d=c.charCodeAt(x);var h=d.toString(16);
+    o+='%'+(h.length<2?'0':'')+h.toUpperCase();}x++;}}return o;},
+URLDecode:function(s){var o=s;var binVal,t;var r=/(%[^%]{2})/;
+  while((m=r.exec(o))!=null && m.length>1 && m[1]!=''){b=parseInt(m[1].substr(1),16);
+  t=String.fromCharCode(b);o=o.replace(m[1],t);}return o;}
+});
+
+/***********************/
+/* The awesome console */
+/***********************/
+
+function log( message, style) {
+	//Define the console to display messages
+	// 3 "styles" for the messages can be used, defined in the CSS via classes: default, success & error
+	if(!style){ var style = "default"; }
+	var ourMessage = $('<div/>').html(message).addClass(style);
+	$('#log').prepend(ourMessage);
+}
+
+/* Console behaviour */
+$("#log a").click(function(){
+	$(this).parent().toggle();
+	$("#showLogs").toggle();
+})
+$("#showLogs").click(function(){
+	$(this).toggle();
+	$("#log").toggle();
+})
+
+/****************************/
+/* Setup of event listening */
+/****************************/
+
+myNose = new Nose();
+
+//Attach custom event to listen to updates
+$('body').bind('noseFetch', function(){
+	myNose.render();
+})
+$('body').bind('updateLinkedData', function(){
+	$('#data').html(myNose.rendering);
+});
+
+/****************************/
+/* Autocompletion behaviour */
+/****************************/
+
+$('#movie').autocomplete({
+	//Thanks to the JQuery UI, let's implement autocompletion
+	source: function(request, response) {
+		$.ajax({
+			//base URL for the Freebase API
+			url: 'http://freebase.com/api/service/search?',
+			//We want JSON data returned
+			dataType: 'jsonp',
+			//We force a request stricly restricted to films
+			data: { query: request.term, type: '/film/film', type_strict: 'all', limit: 3 },
+			success: function(data) {
+				response($.map(data.result, function(item) {
+					return {
+						label: item.name + ' http://freebase.com' + item.id,
+						value: 'http://www.freebase.com/experimental/topic/standard' + item.id
+					}
+				}));
+			}
+		});
+	},
+	minLength: 2,
+	select: function(event, ui) {
+	  //When a option of the list is selected
+		if (ui.item) {
+			//Let's clean the canvas: remove the previous results
+			$('table').remove();
+	    var url = ui.item.value;
+	    var name = ui.item.label;
+		  log("You selected" + name);
+		  log("Will now try to fetch data about" + url);
+	  	myNose.follow(url, 'jsonp', ['result','properties','/film/film/soundtrack', 'values'],'soundtracks');
+		}
+	},
+});
+
+//Now, when user clicks on something, let's do the right thing according to what's in there
+$('td').live('click',function(){
+	var value = $(this).html();
+	
+	//All the logic goes here
+	if($('table').hasClass('soundtracks')){
+		var regex = /^http:\/\/www\.freebase\.com\/view\/en\/.{1,}$/;
+		if(regex.test(value)){
+			urlToFollow = "http://www.freebase.com/experimental/topic/standard/en/" + value.substring(value.lastIndexOf("/")+1);  
+			log('Nose will now follow ' + urlToFollow);
+			myNose.follow(urlToFollow, 'jsonp', ['result','properties','/music/album/releases', 'values'],'releases');
+		}
+	}
+	
+	if($('table').hasClass('releases')){
+		var regex = /^http:\/\/www\.freebase\.com\/view\/m\/.{1,}$/;
+		if(regex.test(value)){
+			urlToFollow = "http://www.freebase.com/experimental/topic/standard/m/" + value.substring(value.lastIndexOf("/")+1);  
+			log('Nose will now follow ' + urlToFollow);
+			myNose.follow(urlToFollow, 'jsonp', ['result','webpage'],'release');
+		}
+	}
+	
+	if($('table').hasClass('release')){
+		var regex = /^http:\/\/mb-redir\.freebaseapps\.com\/redir\/.{1,}$/;
+		if(regex.test(value)){
+			urlToFollow = "http%3A%2F%2Fmm.musicbrainz.org%2Fws%2F1%2Frelease%2F" + value.substring(value.lastIndexOf("/")+1) + ".html%3Ftype%3Dxml%26inc%3Dartist%2bcounts%2brelease-events%2bdiscs%2btracks";  			
+			log('Nose will now follow ' + urlToFollow);
+			myNose.follow(urlToFollow, 'xml', ['query','results','metadata','release','track-list','track'],'tracklist');
+		}
+	}
+	
+	if($('table').hasClass('tracklist')){
+		var regex = /^[a-z0-9-]{1,}$/;
+		if(regex.test(value)){
+			urlToFollow = "http%3A%2F%2Fmm.musicbrainz.org%2Fws%2F1%2Ftrack%2F" + value + ".html%3Ftype%3Dxml%26inc%3Dartist";  			
+			log('Nose will now follow ' + urlToFollow);
+			myNose.follow(urlToFollow, 'xml', ['query','results','metadata','track', 'artist'],'artist');
+		}
+	}
+	
+});
+
+//Woohoo! We're all set up. Let's gooo!
+log("hello, it seems I'm ready to use. Welcome folks!");
+
+//testing xml case || working so far !!
+//var encodedUrl = 'http%3A%2F%2Fmm.musicbrainz.org%2Fws%2F1%2Frelease%2Fc8146647-dd7b-40cc-9d46-c53489a3651d.html%3Ftype%3Dxml%26inc%3Dartist%2bcounts%2brelease-events%2bdiscs%2btracks';
+//myNose.follow(encodedUrl, 'xml', ['query', 'results', 'metadata', 'release','track-list','track']);
+
--- js/nose.js
+++ js/nose.js
@@ -1,248 +1,121 @@
-/*
- Script to implement the "Follow your nose" behaviour
-*/
+/***********************/
+/* Nose follower class */
+/***********************/
 
-$(function() {
-	function log( message, style) {
-		//Define the console to display messages
-		// 3 "styles" for the messages can be used, defined in the CSS via classes: default, success & error
-		if(!style){ var style = "default"; }
-		var ourMessage = $('<div/>').html(message).addClass(style);
-		$('#log').prepend(ourMessage);
-	}
+function Nose(){
+	//constructor here. What do we need?
+	this.data = null //once data is fetched, here it goes 
+	this.rendering = '' //variable to hold the rendered HTML
+	this.type = 'neutral' //variable containing the type of data
+}
+Nose.prototype.follow = function(source, dataType, accessPath, type){
+	var self = this;
+	//method to fetch data from a certain source -- MusicBrainz or Freebase -- using Jquery and/or YQL
+	//accessPath variable is an aray containing the keys of the data tree, e.g, ['data','query','results']
 	
-	function displaySoundtrack(tracks,elt){
-		//function to display the soundtracks after the elt passed as argument
-		
-		if(!elt){
-			//If no element was specified, let's append it to the body
-			var elt = $("form");
-		}
-		
-		details = $('<table />').addClass('details');
-		var row;
-		$.each(tracks, function(){
-			row = $('<tr />').html('<td>'+ this.title +'</td><td>'+ this.duration +'</td>'); 
-			details.append(row);
-		})
-		$('<tr />').append(details).insertAfter(elt);
-	}
-	
-	function fetchSoundtrack(id, elt){
-		//This function is here to retrieve the soundtrack info provided an Id like c8146647-dd7b-40cc-9d46-c53489a3651d
-		//It then inserts it after the row of the elt passed as an argument
-		
-		//Let's compute the URL of the release group of the soundtrack
-		var urlBase = 'http%3A%2F%2Fmm.musicbrainz.org%2Fws%2F1%2Frelease%2F';
-		var soundtrackUrl = urlBase + id + '.html%3Ftype%3Dxml%26inc%3Dartist%2bcounts%2brelease-events%2bdiscs%2btracks';
-		
-		//Now, let's build the YQL query
-		var YQLQuery = "select%20*%20from%20xml%20where%20url%3D%22" + soundtrackUrl + "%22";
-		var YQLRestQuery = "http://query.yahooapis.com/v1/public/yql?q=" + YQLQuery + "&format=json&diagnostics=true&callback="
-		
-		log('Computed request for soundtrack: ' + soundtrackUrl);
-		log('YQL Query: ' + YQLRestQuery);
-		
-		$.ajax({
-			url: YQLRestQuery,
-			dataType: 'json',
-			success: function(data) {
-			  if(data){ 
-					if (data.query.results.metadata.release['track-list'].track){
-						var tracks = data.query.results.metadata.release['track-list'].track;
-						if(typeof tracks == 'object' && tracks.length > 0 ){
-							displaySoundtrack(tracks,elt);
-						}
-						else{
-							log('Error: tracks is not an object with size > 0')
-						}
-					}
-					else{
-						log('Error: tracks data has not the right structure');
-					}
+	if(type){ self.type = type; }
+	function dataHandler(data){
+		//General purpose function to, given a certain data and a path, return the interesting part
+		var requestedData;
+		$.each(accessPath, function(){
+				//at each iteration, we go down one level.
+				if(data[this]){
+					requestedData = data[this];
+					data = requestedData;
 				}
-				else{
-					log("No data to work with when fetching the precise release.", 'error');
+				else{ 
+					//!! @ToDo: error handling !! 
+					console.log("Error baby...");
 				}
-			}
 		});
-		
+		//let's store the result in an instance variable
+		self.data = requestedData;
+		console.log('self.data coming');
+		console.log(self.data);
+		//trigger of the event to inform interface that new data is ready
+		$('body').trigger('noseFetch');
 	}
 	
-	function displayReleases(releases){
-		//This function creates a table to diplay the releases
-		log('beginning to write release table to DOM...');
-		var table = $('<table />').addClass('releases');
-		var row = $('<tr />').html('<th>Title</th><th>ID</th><th>Nb Tracks</th>');
-		table.append(row);
-		$.each(releases, function(){
-			row = $('<tr />').html('<td class="title">'+ this.title +'</td><td>'+ this.id +'</td><td>'+ this['track-list'].offset +'</td>');
-			table.append(row);
-		})
-		//Finally, let's append the table to the DOM
-		$('body').append(table);
-		log('table written to DOM. Enjoy.');
-	}
-	
-	function getRelease(url) {
-		//This function's parameter is the URI of a MusicBrainz Release, e.g, 
-		// http://mb-redir.freebaseapps.com/redir/bc04101a-f099-3da6-a89f-41342bab4b80 -- with our Fight Club use case
-		//It will use the ID of this Release group, and fetch a particular release of the soundtrack 
-		
-		//Let's compute the URL of the release group of the soundtrack
-		var urlBase = 'http%3A%2F%2Fmm.musicbrainz.org%2Fws%2F1%2Frelease-group%2F';
-		var id = url.substring(url.lastIndexOf('/') + 1 ); //get the part of the ur after the last "/", i.e, the ID
-		releaseGroupUrl = urlBase + id + '.html%3Ftype%3Dxml%26inc%3Dreleases';
-		
-		//Now, let's build the YQL query
-		var YQLQuery = "select%20*%20from%20xml%20where%20url%3D%22" + releaseGroupUrl + "%22";
-		var YQLRestQuery = "http://query.yahooapis.com/v1/public/yql?q=" + YQLQuery + "&format=json&diagnostics=true&callback="
-		
-		log('Computed release group: ' + releaseGroupUrl);
-		log('YQL Query: ' + YQLRestQuery);
-		
-		$.ajax({
-			url: YQLRestQuery,
-			dataType: 'json',
-			success: function(data) {
-			  if(data){ 
-					if (data.query.results.metadata['release-group']['release-list'].release){
-						var releases = data.query.results.metadata['release-group']['release-list'].release;
-						if(typeof releases == 'object' && releases.length > 0 ){
-							displayReleases(releases);
-						}
-						else if(releases.id){
-							//We're in the case where there is only one soundtrack available, no list to choose from. Let's log that
-							log('Only one soundtrack available so far. Fetching this one directly...');
-							//Now, let's directly display that on the screen.
-							fetchSoundtrack(releases.id);
-						}
-						else{
-							log('Error: releases is not an object with size > 0')
-						}
-					}
-					else{
-						log('Error: release group data has not the right structure');
-					}
-				}
-				else{
-					log("No data to work with when fetching the release group.", 'error');
-				}
-			}
-		});    		      		  
-	}
-	
-	function getSoundtrack(url) {
-		$.ajax({
-			url: url,
-			dataType: 'jsonp',
-			success: function(data) {
-			  if (data.result.webpage) {
-					var text = false;
-					var soundtrackUrl = false;
-					$.each(data.result.webpage, function(){
-					  if(this.text == 'MusicBrainz'){
-							//We found a MusicBrainz reference to follow
-							if(!text && !soundtrackUrl){
-								text = this.text;
-								soundtrackUrl = this.url;
-							}
-						}
-					});
-					if(!text || !soundtrackUrl){ log('Dead-end: the url ' + url + ' has not yield any MusicBrainz results. Sorry about that.', 'error')}
-				  else{ 
-						log('Bingo! Soundtrack found @MusicBrainz: ' + soundtrackUrl, 'success'); 
-						getRelease('http://www.freebase.com/experimental/topic/standard' + soundtrackUrl);
-					}
-					
-				}
-				else{
-					//no data.result.webpage
-					log("Couldn't find data.result.webpage", 'error');
-				}
-			}
-		});    		      		  
-	}
-	
-	function getSoundtrackUrl(url) {
-		$.ajax({
-			url: url,
-			dataType: 'jsonp',
-			success: function(data) {
-			  if (data.result.properties['/film/film/soundtrack'].values[0]) {
-			    var id = data.result.properties['/film/film/soundtrack'].values[0].id;
-				  log("performing getSoundTrack('http://www.freebase.com/experimental/topic/standard" + id + "')")
-				  getSoundtrack('http://www.freebase.com/experimental/topic/standard' + id);
-				  }
-			}
-		});    		  
-	}
-	
-	/* End of functions definitions */
-	/* App is ready to use */
-	log("hello, it seems I'm ready to use. Welcome folks!");
-	
-	/* Autocompletion behaviour setup */
-	$('#movie').autocomplete({
-		//Thanks to the JQuery UI, let's implement autocompletion
-		source: function(request, response) {
+	switch( dataType ){
+		case 'jsonp':
+			//old classic cross-browser AJAX call via JQuery			
 			$.ajax({
-				//base URL for the Freebase API
-				url: 'http://freebase.com/api/service/search?',
-				//We want JSON data returned
-				dataType: 'jsonp',
-				//We force a request stricly restricted to films
-				data: { query: request.term, type: '/film/film', type_strict: 'all', limit: 3 },
-				success: function(data) {
-					response($.map(data.result, function(item) {
-						return {
-							label: item.name + ' http://freebase.com' + item.id,
-							value: 'http://www.freebase.com/experimental/topic/standard' + item.id
-						}
-					}));
-				}
+				url: source,
+				dataType: dataType,
+				success: dataHandler
 			});
-		},
-		minLength: 2,
-		select: function(event, ui) {
-		  //When a option of the list is selected
-			if (ui.item) {
-				//Let's clean the canvas: remove the previous results
-				$('table').remove();
-		    var url = ui.item.value;
-		    var name = ui.item.label;
-			  log("You selected" + name);
-			  log("Will now try to fetch data about" + url);
-			  getSoundtrackUrl(url);
-		  }
-		},
-	});
+		case 'xml':
+			//Access-Control-Allow-Origin problem -- Let's use YQL to fetch the data
+			
+			//Todo: implement the URLencoding
+			encodedUrl = source;
+			//Now, let's build the YQL query
+			var YQLQuery = "select%20*%20from%20xml%20where%20url%3D%22" + encodedUrl + "%22";
+			var YQLRestQuery = "http://query.yahooapis.com/v1/public/yql?q=" + YQLQuery + "&format=json&diagnostics=true&callback="
+			log('YQL Query: ' + YQLRestQuery);
+
+			$.ajax({
+				url: YQLRestQuery,
+				dataType: 'json',
+				success: dataHandler
+			});
+		
+	}
 	
-	/* Console behaviour */
-	$("#log a").click(function(){
-		$(this).parent().toggle();
-		$("#showLogs").toggle();
-	})
-	$("#showLogs").click(function(){
-		$(this).toggle();
-		$("#log").toggle();
-	})
+}
+Nose.prototype.render = function(){
+	//This method transforms this.data into HTML table
+	var self = this;
+	// The 2 functions CreateTableView() and CreateDetailView() are adapted from Zachary Hunter 
+	// Source : http://www.zachhunter.com/2010/04/json-objects-to-html-table/
+	function CreateTableView(objArray) {
+			console.log(objArray);
+	    var str = '<table class="tableView '+ self.type +'">';
+
+	    // table head
+	    str += '<thead><tr>';
+	    for (var index in objArray[0]) {
+	        str += '<th scope="col">' + index + '</th>';
+	    }
+	    str += '</tr></thead>';
+
+	    // table body
+	    str += '<tbody>';
+	    for (var i = 0; i < objArray.length; i++) {
+	        str += (i % 2 == 0) ? '<tr class="alt">' : '<tr>';
+	        for (var index in objArray[i]) {
+	            str += '<td>' + objArray[i][index] + '</td>';
+	        }
+	        str += '</tr>';
+	    }
+	    str += '</tbody>'
+	    str += '</table>';
+	    return str;
+	}
+	function CreateDetailView(objArray) {
+
+	    var str = '<table class="detailView">';
+	    str += '<tbody>';
+
+	    for (var i = 0; i < objArray.length; i++) {
+	        var row = 0;
+	        for (var index in objArray[i]) {
+	            str += (row % 2 == 0) ? '<tr class="alt">' : '<tr>';
+
+	            //table "head"
+	            str += '<th scope="row">' + index + '</th>';
+
+	            str += '<td>' + objArray[i][index] + '</td>';
+	            str += '</tr>';
+	            row++;
+	        }
+	    }
+	    str += '</tbody>'
+	    str += '</table>';
+	    return str;
+	}
 	
-	
-	/* expand table and crunch it when user clicks on the rows */
-	$(".title").live('click', function(){
-		if($(this).hasClass('expanded')){
-			//We want to minify the table
-			$('.details').remove();
-			$(this).removeClass('expanded');
-		}
-		else{
-			//We fetch the list of tracks corresponding to the click
-			fetchSoundtrack($(this).next().html(),$(this).parent()) //we pass as a param: the ID in the second row, and the row after wich we want this to be displayed
-			$(this).addClass('expanded');
-		}
-	})
-	
-	
-	
-});
+	console.log('Now rendering...');
+	this.rendering = CreateTableView(this.data);
+	$('body').trigger('updateLinkedData');
+}
